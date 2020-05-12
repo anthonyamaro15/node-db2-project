9140042c4732723f79bbf57333681fa698333620
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-05-12 14:55:44 -0700

    added seeds

--- data/migrations/20200512134132_dealer.js
+++ data/migrations/20200512134132_dealer.js
@@ -1,15 +1,27 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("dealer", (table) => {
-    table.increments();
-    table.string("VIN", 255).notNullable().unique();
-    table.string("make", 128).notNullable();
-    table.string("model", 128).notNullable();
-    table.integer("mileage").notNullable();
-    table.string("transmition_type", 128);
-    table.string("status", 128);
-  });
+  return knex.schema
+    .createTable("dealer", (table) => {
+      table.increments();
+      table.string("VIN", 255).notNullable().unique();
+      table.string("make", 128).notNullable();
+      table.string("model", 128).notNullable();
+      table.integer("mileage").notNullable();
+      table.string("transmition_type", 128);
+      table.string("status", 128);
+    })
+    .createTable("sales", (table) => {
+      table.increments();
+      table
+        .integer("car_id")
+        .unsigned()
+        .notNullable()
+
+        .references("dealer.id")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    });
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTableIfExists("dealer");
+  return knex.schema.dropTableIfExists("sales").dropTableIfExists("dealer");
 };
