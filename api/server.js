7eed4fc83de8d82892d27b89c44562941f49835b
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-05-12 13:33:12 -0700

    added dependencies needed and boiler plate app

---
+++ api/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const carsRoute = require("../car-routes/carsRoute");
+
+const server = express();
+
+server.use(express.json());
+server.use("/", (req, res) => {
+  res.json("application started");
+});
+server.use("/api/cars", carsRoute);
+
+module.exports = server;
